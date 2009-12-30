d56dc03367a0fedc954b8f951bfa7922b9b4aa7f
Marcio Galli <marciogalli@taboca.local>
2009-12-29 19:25:49 -0800

    SlideBar code - every selection on the page creates a new page panel in the slidebar

--- src/xwid.js
+++ src/xwid.js
@@ -3,44 +3,24 @@
 ///
 //
 jetpack.future.import("selection");   // https://wiki.mozilla.org/Labs/Jetpack/JEP/12
-jetpack.future.import("slideBar");    // https://wiki.mozilla.org/Labs/Jetpack/JEP/16
+jetpack.future.import("slideBar");   
 
-jetpack.selection.onSelection(function regionCapture() {
-
-    jetpack.selection.html = "<span style='border:2px solid black;' id='selsel'>" + jetpack.selection.html + "</span>";
-    var currDoc = jetpack.tabs.focused.contentDocument;
-    var currWin = jetpack.tabs.focused.contentWindow;
-    var embedBox = currDoc.getElementById("selsel").getBoundingClientRect(); 
-    x = embedBox.left;
-    y = embedBox.top;
-    w = parseInt(embedBox.width);
-    h = parseInt(embedBox.height);
-
-    ////
-    /// This is a selection capture that allows for canvas window capture 
-    /// for the selection range of a page. 
-    //
-    setTimeout("xWid.launchForGrab("+currWin+","+x+","+y+","+w+","+h+")", 2000 );
-
-});
-
-////
-/// xWid is a general widget behavior. At this point we have things ( all the widget behaviors ) hardcoded 
 /// in this same file. This may change in the future. 
 //
 var xWid = { 
 
+   canvas: null, 
+   canvasTab: null, 
+ 
    launchForGrab : function (currWin,x,y,w,h){
-    var t = this, canvas;
-    t.tabProperties = new Array();
-    t.tabsIndex = new Array();
-    t.tabProperties = jetpack.tabs.open("about:blank");
-    t.tabProperties.focus();
-    t.tabProperties.onReady( function(doc){
+    this.canvasTab = jetpack.tabs.open("about:blank");
+    this.canvasTab.focus();
+
+    this.canvasTab.onReady( function(doc){
       jQuery(doc.createElementNS("http://www.w3.org/1999/xhtml", "style")).appendTo(jQuery("head",doc)).append("body{background:#131313;color:white;text-align:center;} canvas{cursor:pointer;opacity:0.5;}canvas:hover{opacity:1;}");
-     canvas = doc.createElementNS("http://www.w3.org/1999/xhtml", "canvas");
-     jQuery(canvas).appendTo(jQuery("body",doc));
-     xWid.createPreviewFromArea( currWin ,x,y,w,h, canvas);
+     this.canvas = doc.createElementNS("http://www.w3.org/1999/xhtml", "canvas");
+     jQuery(this.canvas).appendTo(jQuery("body",doc));
+     xWid.createPreviewFromArea( currWin ,x,y,w,h, this.canvas);
 
     });
   }, 
@@ -61,13 +41,31 @@ var xWid = {
 		onClick: function(slide) {
 			slide.icon.src = "chrome://branding/content/icon48.png";
 		}
-	);
+	});
   } 
-
 
 }  // End of general widget code 
 
 
-
 xWid.init();
+
+jetpack.selection.onSelection(function regionCapture() {
+
+    jetpack.selection.html = "<span style='border:2px solid black;' id='selsel'>" + jetpack.selection.html + "</span>";
+    var currDoc = jetpack.tabs.focused.contentDocument;
+    var currWin = jetpack.tabs.focused.contentWindow;
+    var embedBox = currDoc.getElementById("selsel").getBoundingClientRect(); 
+    x = embedBox.left;
+    y = embedBox.top;
+    w = parseInt(embedBox.width);
+    h = parseInt(embedBox.height);
+
+    ////
+    /// This is a selection capture that allows for canvas window capture 
+    /// for the selection range of a page. 
+    //
+    //setTimeout("xWid.launchForGrab("+currWin+","+x+","+y+","+w+","+h+")", 2000 );
+    xWid.init();
+
+});
 
