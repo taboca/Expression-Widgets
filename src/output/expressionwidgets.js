a0e3f401fbc3644db42b16609f951c0e1f539147
Marcio Galli <marciogalli@taboca.local>
2010-01-29 23:53:59 -0800

    Build 0.2 candidate

--- src/output/expressionwidgets.js
+++ src/output/expressionwidgets.js
@@ -1083,9 +1083,9 @@ xWid.overlay = {
 xWid.transport = libCataliser_post; 
 
 // Enable this to disable debugging 
-//xWid.dump = function () { } 
+xWid.dump = function () { } 
 
-xWid.cssStack_slidebar.push("#debug {  margin:auto; width:90%; padding:.2em; margin-top:.5em; -moz-box-shadow: black 0 0 10px; -moz-border-radius:10px; width:94%; background-image: -moz-linear-gradient(top, #555, #555); display:none;  display:block; font-size:80%; color: white; } ");
+//xWid.cssStack_slidebar.push("#debug {  margin:auto; width:90%; padding:.2em; margin-top:.5em; -moz-box-shadow: black 0 0 10px; -moz-border-radius:10px; width:94%; background-image: -moz-linear-gradient(top, #555, #555); display:none;  display:block; font-size:80%; color: white; } ");
 
 xWid.cssStack_slidebar.push(".frame { width:1px; height:1px; position:absolute; left:-10px } ");
 
