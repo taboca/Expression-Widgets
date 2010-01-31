ef60551b59e2ba3f5225b7cf6bd21e7ed7356e5f
Marcio Galli <marciogalli@taboca.local>
2010-01-30 17:56:28 -0800

    0.4 release

--- expressionwidgets.js
+++ expressionwidgets.js
@@ -382,6 +382,10 @@ xWid.digester = {
                 var hh = this.time_getHour(); 
                 var mm = this.time_getMin(); 
                 var ss = this.time_getSec(); 
+
+		if(parseInt(ss)<10) { ss="0"+ss } 
+		if(parseInt(mm)<10) { mm="0"+mm } 
+		if(parseInt(hh)<10) { hh="0"+hh } 
                 
                 var hour = hh+":"+mm+":"+ss; 
 
@@ -1099,12 +1103,12 @@ xWid.overlay = {
 			for (var key in this.rawStore) { 
 				let currLine = this.rawStore[key];
 				currLine = jQuery.trim(currLine);
-				let dataChunks  = currLine.split("::"); 
-				let metaChunks  = dataChunks[0].split("  ");
-				let stampChunks = metaChunks[0].split(" ");
+				var dataChunks  = currLine.split("::"); 
+				var metaChunks  = dataChunks[0].split("  ");
+				var stampChunks = metaChunks[0].split(" ");
 				if(stampChunks.length>=2) { 
-					let date = stampChunks[0]; 
-					let hour = stampChunks[1]; 
+					let date = stampChunks[1]; 
+					let hour = stampChunks[2]; 
 					var contentData = dataChunks[1]; 
 					var appData     = metaChunks[1];;
 					this.addStore(date, hour, appData, contentData); 
@@ -1115,13 +1119,21 @@ xWid.overlay = {
 	}, 
 
 	addStore: function ( date, hour, app, data) {
+
                 var nodeEntry = {
                        date: date,
                        hour: hour,
                        app : app,
                        data: data
                 }
-                this.storeIndex[date+hour +"-"+Math.random()] = nodeEntry;
+
+		var dd = date.split("-");
+		var hh = hour.split(":");
+		var d1 = dd[0]+dd[1]+dd[2];
+		var h1 = hh[0]+hh[1]+hh[2];
+                //this.storeIndex[date+"-"+hour] = nodeEntry;
+xWid.dump("["+d1+h1+"]");
+                this.storeIndex["key"+d1+h1] = nodeEntry;
         },
 
 
