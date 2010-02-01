556efdbf40bc41946ce4b41f7769d6862ab318a7
Marcio Galli <marciogalli@taboca.local>
2010-01-31 23:50:14 -0800

    respin again .5.5

--- expressionwidgets.js
+++ expressionwidgets.js
@@ -1,7 +1,7 @@
 //////
 /////
 ////         Expression Widgets - JetPack for Learning 2010
-/// 0.5.3
+/// 0.5.5
 //
 
 /* JEP Profile Disclaimer 
@@ -213,7 +213,7 @@ xWid.init();
 */
 
 xWid.resources = { 
-    html_panel     : "<div id='topheader'><button alt='Visit the project' id='goproject' href='javascript:'>ExpressionWidgets 0.5.3</button><img align='top' width='30' id='loadingfeedback' src='chrome://global/skin/media/throbber.png'></div><table id='mainpanel'><tr><td><button id='goinit'>Login</button></td><td><input id='login' type='text' /></td></tr><tr><td><button id='goclass' title='Visit the Wiki Class URL in overlay mode' >Class:</button></td><td><input id='repository' type='text' /></td></tr><tr><td align='center' colspan='2' id='topactions'><button id='gosave' disabled='disabled'>Save wiki</button></td></tr></table><div id='notificationpanel'></div> <div id='widgetspanel'></div><div id='widgetscanvas'></div> <div id='historybgcontainer'><div id='historycontainer'><div id='historypanel'></div></div></div> <div id='debug'></div>", 
+    html_panel     : "<div id='topheader'><button alt='Visit the project' id='goproject' href='javascript:'>ExpressionWidgets 0.5.5</button><img align='top' width='30' id='loadingfeedback' src='chrome://global/skin/media/throbber.png'></div><table id='mainpanel'><tr><td><button id='goinit'>Login</button></td><td><input id='login' type='text' /></td></tr><tr><td><button id='goclass' title='Visit the Wiki Class URL in overlay mode' >Class:</button></td><td><input id='repository' type='text' /></td></tr><tr><td align='center' colspan='2' id='topactions'><button id='gosave' disabled='disabled'>Save wiki</button></td></tr></table><div id='notificationpanel'></div> <div id='widgetspanel'></div><div id='widgetscanvas'></div> <div id='historybgcontainer'><div id='historycontainer'><div id='historypanel'></div></div></div> <div id='debug'></div>", 
     html_login_helper: "<div id='helper'><img src='chrome://global/skin/notification/warning-icon.png' /> You are not logged in. Log over the wiki and then click here <button id='gotry'>Retry</button> </div>",
     html_login_nouser_helper: "<div id='helper'><img src='chrome://global/skin/notification/warning-icon.png' /> You don't have the username/wiki settings. Please follow the instructions on how to set your user section in the wiki class repository.  <button id='gotry'>Retry</button> <button style='margin:.5em' id='gohelp'>Setup/Help Instructions</button></div>",
     html_login_noinfo_helper: "<div id='helper'><img src='chrome://global/skin/notification/warning-icon.png' /> You don't have the username/wiki settings. Please follow the instructions on how to set your user section in the wiki class repository. <button style='margin:1em' id='gohelp'>Instructions</button></div>",
@@ -316,7 +316,7 @@ xWid.digester = {
 		var preParse = this.userContent.split("===");
 		if (preParse.length==3) { 
 			xWid.dump("Found user..");
-			var userData = preParse[1].split("*"); 
+			var userData = preParse[2].split("*"); 
 			this.storeIndex = new Array();
 			for (var key in userData) { 
 				let currLine = userData[key];
@@ -923,7 +923,7 @@ widgets.selection = {
   },
 
   init: function () { 
-        jQuery("#widgetscanvas",this.slideDoc).html("<div class='selection'>Type text or select from browser: <br /><textarea id='widget_selection_field'  /><br /><button id='widget_selection_send'>Send</button></div>");
+        jQuery("#widgetscanvas",this.slideDoc).html("<div class='selection'>Type text or select from browser: <br /><input id='widget_selection_field'  /><br /><button id='widget_selection_send'>Send</button></div>");
 	jQuery("#widgetscanvas",this.slideDoc).css("display","block");
 	jQuery("#widgetscanvas",this.slideDoc).css("background-color","#cec");
 	refThis = this; 
