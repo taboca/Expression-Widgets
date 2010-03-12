cd332f3632cd6b1cf197036a4db5d25b662f6fcf
Marcio Galli <marciogalli@taboca.local>
2010-03-11 16:54:04 -0800

    Doc/help

--- script/guide.js
+++ script/guide.js
@@ -7,13 +7,27 @@
 
 $(document).ready(function() {
      $('.wizard-panel').css('display','none');
+     $('.wizard-button').onClick( function (refEl) { 
+	
+	 var currentButton = $(refEl).attr("id");
+	 var command       = currentButton.split("wizard-button-")[1].toString();
+	 execCommand(command);
+		
+     });
+
      $('.wizard-button').removeClass('wizard-selected');
      var s= document.location.toString();
      var command = s.split("#show=")[1];
      if(command == undefined) { 
 	command = "project";
      }
+
+     execCommand(command);
+
+});
+
+function execCommand(str) { 
      $("#wizard-panel-"+command).css("display","block");
      $('#wizard-button-'+command).addClass('wizard-selected');
-});
+} 
 
